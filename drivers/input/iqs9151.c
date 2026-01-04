99f8af26155aa4f8dc9f3413d685b2a9843f81ac
ShiniNet <90338118+ShiniNet@users.noreply.github.com>
2026-01-04 18:12:57 +0800

    add template

--- drivers/input/iqs9151.c
+++ drivers/input/iqs9151.c
@@ -4,11 +4,11 @@
  * To be implemented.
  */
 
-#include <zephyr/device.h>
+#include <zephyr/pm/device.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
-#include "iqs9151.h"
+#include "iqs9151_init.h"
 
 /* Implementation will be added later */
